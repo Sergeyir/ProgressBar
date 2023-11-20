b417fb81fbc35be048f5a824c7edb410f4072913
Sergey <antsupov0124@gamil.com>
2023-11-20 16:50:33 +0300

    adding namespace to tools for ProgressBar

--- lib/StrTool.h
+++ lib/StrTool.h
@@ -3,16 +3,19 @@
 #include <sstream>
 #include <iomanip>
 
-std::string FtoStr(float val, const int precision = 2)
+namespace ProgressBarTools
 {
-	std::stringstream ssval;
-	ssval << std::fixed << std::setprecision(precision) << val;
-	return ssval.str();
-}
+	std::string FtoStr(float val, const int precision = 2)
+	{
+		std::stringstream ssval;
+		ssval << std::fixed << std::setprecision(precision) << val;
+		return ssval.str();
+	}
 
-std::string DtoStr(float val, const int precision = 2)
-{
-	std::stringstream ssval;
-	ssval << std::fixed << std::setprecision(precision) << val;
-	return ssval.str();
+	std::string DtoStr(float val, const int precision = 2)
+	{
+		std::stringstream ssval;
+		ssval << std::fixed << std::setprecision(precision) << val;
+		return ssval.str();
+	}
 }
