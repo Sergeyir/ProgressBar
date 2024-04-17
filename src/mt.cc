a1f5dd545a69436e5ff9d043d4c0f51ef7f8ab31
Sergeyir <antsupov0124@gmail.com>
2024-04-17 11:50:05 +0300

    better multithread progress bar display

--- src/mt.cc
+++ src/mt.cc
@@ -11,24 +11,40 @@ void do_something()
 
 int main()
 {
-	ProgressBar pbar_mt = ProgressBar("FANCY", "Multithread");
 	const auto nthreads = std::thread::hardware_concurrency();
 
 	std::cout << "Running this program on " << nthreads << " threads" << std::endl;
 
 	double ncalls = 0;
 	const double max_ncalls = 5e2*nthreads;
+
+	bool process_finished = false;
 	
 	auto ThrCall = [&]()
 	{
 		for (int i = 0; i < 5e2; i++)
 		{
-			pbar_mt.Print(ncalls/max_ncalls);
-			
 			do_something();
 			ncalls += 1.;
 		}
 	};
+
+	auto PbarCall = [&]()
+	{
+		ProgressBar pbar_mt = ProgressBar("FANCY", "Multithread");
+		while (!process_finished)
+		{
+			pbar_mt.Print(ncalls/max_ncalls);
+			std::this_thread::sleep_for(std::chrono::milliseconds(20));
+		}
+		pbar_mt.Print(1.);
+	};
+
+	//pbar needs to have it's own thread for it 
+	//to be consistent as frequent overwriting on
+	//multiple cores leads to interference and 
+	//therefore the progress bar is displayed incorrectly
+	std::thread pbar_thread(PbarCall);
 	
 	std::vector<std::unique_ptr<std::thread>> thr;
 	
@@ -36,14 +52,15 @@ int main()
 	{
 		thr.push_back(std::unique_ptr<std::thread>(new std::thread(ThrCall)));
 	}
-
+	
 	while (!thr.empty()) 
 	{
 		thr.back()->join(); 
 		thr.pop_back();
 	}
 
-	pbar_mt.Print(1.);
+	process_finished = true;
+	pbar_thread.join();
 
 	return 0;
 }
